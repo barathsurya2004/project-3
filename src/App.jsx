992dca96e564b8bf959b84efae76cf528b9f4bf1
Zeus Crusher <barathsurya2004@gmail.com>
2024-06-14 17:41:03 +0530

    done with face shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import { Canvas } from "@react-three/fiber";
-import { AnimTest } from "../public/models/AnimTest";
-import Face from "./pages/Face";
-import Hero from "./pages/Hero";
-
+import FaceReact from "./pages/FaceReact";
+import bg from "./assets/background.png";
+import { FaceModel } from "../public/models1/Face";
+import { OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
 const App = () => {
   return (
-    <Canvas>
-      <AnimTest />
-    </Canvas>
+    <>
+      <img
+        src={bg}
+        alt=""
+        style={{
+          position: "absolute",
+          zIndex: 0,
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
+      <FaceReact />
+      <div
+        className="face-model"
+        style={{
+          width: "100%",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          zIndex: 2,
+          left: 0,
+        }}
+      >
+        <Canvas>
+          <ambientLight intensity={1} />
+          <directionalLight intensity={3} position={[10, 10, 10]} />
+          <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={100} />
+          {/* <PerspectiveCamera makeDefault position={[0, 0, 10]} zoom={0.4} /> */}
+          <FaceModel />
+        </Canvas>
+      </div>
+    </>
   );
 };
 
