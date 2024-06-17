289e8762abcaea2381ca6fedccfd6596b95e386f
Zeus Crusher <barathsurya2004@gmail.com>
2024-06-17 18:02:58 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
 import { Canvas } from "@react-three/fiber";
-import FaceReact from "./pages/FaceReact";
-import bg from "./assets/background.png";
-import { FaceModel } from "../public/models1/Face";
-import { OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import React, { useEffect } from "react";
+import { Face } from "../public/models/Face1";
+import { CameraControls, OrthographicCamera } from "@react-three/drei";
+
 const App = () => {
+  useEffect(() => {
+    window.addEventListener("devicemotion", (e) => {
+      console.log(e);
+    });
+  });
   return (
     <>
-      <img
-        src={bg}
-        alt=""
-        style={{
-          position: "absolute",
-          zIndex: 0,
-          width: "100vw",
-          height: "100vh",
-        }}
-      />
-      <FaceReact />
-      <div
-        className="face-model"
-        style={{
-          width: "100%",
-          height: "100vh",
-          position: "fixed",
-          top: 0,
-          zIndex: 2,
-          left: 0,
-        }}
-      >
-        <Canvas>
-          <ambientLight intensity={1} />
-          <directionalLight intensity={3} position={[10, 10, 10]} />
-          <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={50} />
-          {/* <PerspectiveCamera makeDefault position={[0, 0, 10]} zoom={0.4} /> */}
-          <FaceModel />
-        </Canvas>
+      <div>
+        <h1>React Three Fiber</h1>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100vh",
+          }}
+        >
+          <Canvas>
+            <ambientLight />
+            <directionalLight intensity={3} position={[10, 10, 10]} />
+            <OrthographicCamera makeDefault position={[0, 0, 20]} zoom={10} />
+            <CameraControls makeDefault />
+            <Face />
+          </Canvas>
+        </div>
       </div>
     </>
   );
