c29d11c13b6853bb9f7157fd05e14428f2816e34
Zeus Crusher <barathsurya2004@gmail.com>
2024-06-29 14:28:50 +0530

    added cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,24 @@
 import { Canvas } from "@react-three/fiber";
-import React, { useEffect } from "react";
-
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Globe } from "../public/models/Globe";
-import { Earth } from "../public/models/Earth";
-import Hero from "./pages/Hero";
+import SvgMorphAnimation from "./pages/SvgMorphAnimation";
+import { Blender } from "../public/models/Blender";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 
 const App = () => {
   return (
-    <>
-      <div>
-        <h1>React Three Fiber</h1>
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100vh",
-          }}
-        >
-          <Canvas
-            gl={{
-              physicallyCorrectLights: true,
-              powerPreference: "high-performance",
-              antialias: true,
-              // depth: true,
-            }}
-          >
-            {/* <ambientLight intensity={2} /> */}
-            <directionalLight intensity={2} position={[10, 10, 10]} />
-            <PerspectiveCamera
-              makeDefault={true}
-              far={1000}
-              near={0.001}
-              fov={22.895}
-              position={[0, 0, 2.212]}
-            />
-            <OrbitControls />
-            <Earth />
-          </Canvas>
-        </div>
-      </div>
-    </>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Canvas>
+        <directionalLight position={[5, 5, 5]} intensity={3} />
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={100} />
+        <Blender />
+      </Canvas>
+    </div>
   );
 };
 
